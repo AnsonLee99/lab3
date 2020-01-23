1e153e9409ff5ac1ebc1cd6d19e28856fa52bafa
Anson <all197@ucsd.edu>
2020-01-23 00:35:02 -0800

    configured express server

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,16 @@
  */
 
 exports.view = function(req, res){
+  var nameToShow = req.params.userName;
+
+  if (nameToShow === undefined) {
+    nameToShow = 'World';
+  }
+
+  console.log("NAME IS: ", nameToShow);
+  console.log(req.params)
+
   res.render('index', {
-  	'name': '',
+  	'name': nameToShow,
   });
 };
